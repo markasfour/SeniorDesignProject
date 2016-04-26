96e1cbee7495e553ce73d04c1bbfc21098c65134
Mark Asfour <mj4sparrow@gmail.com>
2016-04-25 19:30:56 -0700

    added map style customization

--- map.js
+++ map.js
@@ -10,9 +10,72 @@ var map = L.map('mapid', {
 				'maxBounds': maxBounds
 				}).fitBounds(maxBounds);
 
-L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//load all the maps
+//default map
+var default_map = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+ 
+//geographic map
+var terrain_map = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/{type}/{z}/{x}/{y}.{ext}', {
+	type: 'sat',
+	ext: 'jpg',
+	attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency',
+	subdomains: '1234'
+}); 
+
+//dark map
+var dark_map = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+});
+
+//grayscale map
+var grayscale_map = L.tileLayer('http://korona.geog.uni-heidelberg.de/tiles/roadsg/x={x}&y={y}&z={z}', {
+	maxZoom: 19,
+	attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+
+//night time map
+var night_map = L.tileLayer('http://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}', {
+	attribution: 'Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href="https://earthdata.nasa.gov">ESDIS</a>) with funding provided by NASA/HQ.',
+	bounds: [[-85.0511287776, -179.999999975], [85.0511287776, 179.999999975]],
+	minZoom: 1,
+	maxZoom: 8,
+	format: 'jpg',
+	time: '',
+	tilematrixset: 'GoogleMapsCompatible_Level'
+});
+
+var map_list = [default_map, terrain_map, dark_map, grayscale_map, night_map];
+var cur_map = 0;
+
+function setMapType(map_type) {
+	map.removeLayer(map_list[cur_map]);
+	if (map_type === 'default')  {
+		cur_map = 0;
+	}
+	else if (map_type === 'terrain') {
+		cur_map = 1;
+	}
+	else if (map_type === 'dark') {
+		cur_map = 2;
+	}
+	else if (map_type === 'grayscale') {
+		cur_map = 3;
+	}
+	else if (map_type === 'nighttime') {
+		cur_map = 4;
+	}
+	map_list[cur_map].addTo(map);
+}
+
+function changeMapType(map_types){
+	var map_type = map_types.map.value;
+	setMapType(map_type);
+}
 
 
 //THIS COLORS THE MAP	
