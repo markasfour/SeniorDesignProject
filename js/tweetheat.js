3df81d7fcaae78eb4425bc470fb2b4722ae9a0dc
Mehran Ghamaty <mnghamaty@gmail.com>
2016-05-09 00:25:11 -0700

    rmed files

--- js/tweetheat.js
+++ js/tweetheat.js
@@ -1,10 +1,49 @@
 var keywordURL = "https://ghamaty.tech:5000/maprest/apv1/get/keywords"
+var twitterURL = keywordURL + "/twitter"
+var googleHotURL = keywordURL + "/google/search"
+var googleSearchURL = keywordURL + "/google/hot"
 var tweetheat = angular.module('tweetheat', ['ngAnimate']);
 
 tweetheat.factory('heatFactory', function(){
   //used to get the keywords
   function getKeywords(){
     var url = keywordURL;  
+    return $http({
+      method: 'GET',
+      url: url,
+      dataType: 'jsonp',
+      headers: {'Accept': 'application/json'}
+    }).then(function (result) {
+      return result.data;
+    });
+  }
+
+  function getTwitter(){
+    var url = twitterURL;  
+    return $http({
+      method: 'GET',
+      url: url,
+      dataType: 'jsonp',
+      headers: {'Accept': 'application/json'}
+    }).then(function (result) {
+      return result.data;
+    });
+  }
+
+  function getGoogleHot(){
+    var url = googleSearchURL;  
+    return $http({
+      method: 'GET',
+      url: url,
+      dataType: 'jsonp',
+      headers: {'Accept': 'application/json'}
+    }).then(function (result) {
+      return result.data;
+    });
+  }
+
+  function getGoogleSearch(){
+    var url = googleSearchURL;  
     return $http({
       method: 'GET',
       url: url,
