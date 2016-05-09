097079ad0fcc69e7ff1a9927c6f6cbf01e6e3902
Mark Asfour <mj4sparrow@gmail.com>
2016-05-09 01:03:03 -0700

    started about page

---
+++ js/about_move.js
@@ -0,0 +1,13 @@
+var about_move = function() {
+    $('#icon-about').click(function()
+    {
+        $('#about').animate({left: '0%'}, 200);
+    });
+    
+    $('#icon-close-about').click(function()
+    {
+        $('#about').animate({left: '-100%'}, 200);
+    });
+};
+
+$(document).ready(about_move)
