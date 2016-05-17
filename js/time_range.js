0ad01b77926adf977b51562efca245b4c7732e37
Mark Asfour <mj4sparrow@gmail.com>
2016-05-17 12:45:33 -0700

    added time range slider

---
+++ js/time_range.js
@@ -0,0 +1,18 @@
+var timeSlider = document.getElementById('time-range');
+
+noUiSlider.create(timeSlider, {
+	start: [1, 24],
+	connect: true,
+	step: 1,
+	direction: 'rtl',
+	range: {
+		'min': [ 1 ],
+		'max': [ 24 ]
+	}
+});
+
+var timeValues = [document.getElementById('time-range-value1'), document.getElementById('time-range-value2')];
+
+timeSlider.noUiSlider.on('update', function( values, handle ) {
+		timeValues[handle].innerHTML = values[handle];
+});
